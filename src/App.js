313b68e669c3e4b597985fdd6e1a28af9a0aef31
unknown <mehedi5051@gmail.com>
2020-04-26 15:29:49 +0600

    API Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import CoronaContextProvider from './context/coronaState'
-import CoronaVirus from './components/CoronaVirus';
-import Header from './components/Header';
-import Search from './components/Search';
-import Total from './components/Total';
+import React from "react";
+import "./App.css";
+import CoronaContextProvider from "./context/coronaState";
+import CoronaVirus from "./components/CoronaVirus";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Total from "./components/Total";
 
 function App() {
-  return (
-    <div className="App">
-     <CoronaContextProvider>
-       <Header />
-       <Search />
-       <Total />
-       <CoronaVirus />
-     </CoronaContextProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<CoronaContextProvider>
+				<Header />
+				<Search />
+				<Total />
+				<CoronaVirus />
+			</CoronaContextProvider>
+		</div>
+	);
 }
 
 export default App;
--- src/context/coronaState.js
+++ src/context/coronaState.js
@@ -1,51 +1,54 @@
-import React, { useReducer } from 'react'
-import axios from 'axios'
-import CoronaContext from './coronaContext'
-import coronaReducer from './coronaReducer'
+import React, { useReducer } from "react";
+import axios from "axios";
+import CoronaContext from "./coronaContext";
+import coronaReducer from "./coronaReducer";
 
-import { GET_CORONA_UPDATE, FIND_BY_COUNTRY, SET_TOTAL} from './types'
+import { GET_CORONA_UPDATE, FIND_BY_COUNTRY, SET_TOTAL } from "./types";
 
-const CoronaContextProvider = props => {
-    const initialState = {
-        locations: [],
-        total: {
-            confirmed: 0,
-            deaths: 0,
-            recovered: 0,
-            updated: 0
-        },
-        searchedCountry: null
-    }
+const CoronaContextProvider = (props) => {
+	const initialState = {
+		locations: [],
+		total: {
+			confirmed: 0,
+			deaths: 0,
+			recovered: 0,
+			updated: 0,
+		},
+		searchedCountry: null,
+	};
 
-    const [state, dispatch] = useReducer(coronaReducer, initialState)
+	const [state, dispatch] = useReducer(coronaReducer, initialState);
 
-    const getCoronaUpdate = async () => {
-        const res = await axios.get('https://corona.lmao.ninja/countries')
-        dispatch({ type: GET_CORONA_UPDATE, payload: res.data })
+	const getCoronaUpdate = async () => {
+		const res = await axios.get("https://corona.lmao.ninja/v2/countries");
+		dispatch({ type: GET_CORONA_UPDATE, payload: res.data });
+	};
+	const getTotal = async () => {
+		const res = await axios.get("https://corona.lmao.ninja/v2/all");
+		dispatch({ type: SET_TOTAL, payload: res.data });
+	};
 
-    }
-    const getTotal = async () => {
-        const res = await axios.get('https://corona.lmao.ninja/all')
-        dispatch({ type: SET_TOTAL, payload: res.data })
-    }
+	const findByCountry = (searchText) => {
+		const country = state.locations.filter((location) =>
+			location.country.toLowerCase().includes(searchText)
+		);
+		dispatch({ type: FIND_BY_COUNTRY, payload: country });
+	};
 
-    const findByCountry = searchText => {
-        const country = state.locations.filter(location => location.country.toLowerCase().includes(searchText))
-        dispatch({ type: FIND_BY_COUNTRY, payload: country })
-    }
+	return (
+		<CoronaContext.Provider
+			value={{
+				locations: state.locations,
+				total: state.total,
+				searchedCountry: state.searchedCountry,
+				getCoronaUpdate,
+				getTotal,
+				findByCountry,
+			}}
+		>
+			{props.children}
+		</CoronaContext.Provider>
+	);
+};
 
-    return(
-        <CoronaContext.Provider value={{
-                locations: state.locations,
-                total: state.total,
-                searchedCountry: state.searchedCountry,
-                getCoronaUpdate,
-                getTotal,
-                findByCountry
-            }}>
-            { props.children }
-        </CoronaContext.Provider>
-    )
-}
-
-export default CoronaContextProvider
+export default CoronaContextProvider;
